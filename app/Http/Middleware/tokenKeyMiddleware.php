dc66e9da5c2e0766c8ece5933402dbcb8dbe9fef
Deepak Kumar <structlooper@gmail.com>
2020-02-20 16:07:41 +0530

    basic auth added

--- app/Http/Middleware/tokenKeyMiddleware.php
+++ app/Http/Middleware/tokenKeyMiddleware.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
-
+use Illuminate\Support\Facades\Auth;
 use Closure;
 
 class tokenKeyMiddleware
@@ -15,11 +15,18 @@ class tokenKeyMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $token = $request->header('token');
-        if ($token == 'ABCDEFGH') {
+        // $token = $request->header('token');
+        // if ($token == 'ABCDEFGH') {
             
+        //     return $next($request);
+        // }
+        // return response()->json(['message' => 'Wrong token please provide correct token key'],404);
+        // $user = Auth::onceBasic();
+        if (Auth::onceBasic()) {
+            return response()->json(['message' => 'User Not Autorised!!'],404);
+        }
+        else {
             return $next($request);
         }
-        return response()->json(['message' => 'Wrong token please provide correct token key'],404);
     }
 }
